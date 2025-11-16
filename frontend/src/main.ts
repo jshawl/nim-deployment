6ba1f2411e6dedefc468fca19a2bb85921a62d38
Jesse Shawl <jesse@jesse.sh>
2025-11-15 19:57:53 -0500

    add vite (#28)
    * add vite
    
    * add newline

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,20 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Vite + TypeScript</h1>
+    <pre></pre>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
 const dateParam = location.hash.slice(1);
 const pre = document.querySelector("pre");
-if (dateParam) {
-  const offset = new Date().getTimezoneOffset();
+if (dateParam && pre) {
   const now = new Date().toString();
-  const tz = now.match(/GMT([^\s]{3})/)[1];
+  const tz = now.match(/GMT([^\s]{3})/)?.[1];
   const parsed = Date.parse(`${dateParam}T00:00:00.000${tz}:00`);
   const from = new Date(parsed).toISOString();
   const to = new Date(parsed + 86400000).toISOString();
