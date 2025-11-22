dc87929e104af9c4eeb89188a44e934546e1910f
Jesse Shawl <jesse@jesse.sh>
2025-11-22 11:10:21 -0500

    add sections (#39)

--- server/tests/test_geohash.nim
+++ server/tests/test_geohash.nim
@@ -10,3 +10,14 @@ test "neighbor":
   check neighbor("9q5", "bottom") == "9mg"
   check neighbor("9q5", "right") == "9qh"
   check neighbor("9q5", "left") == "9q4"
+
+test "sections":
+  let north = 40.7355871786238
+  let east = -73.95777974265621
+  let south = 40.63090494037923
+  let west = -74.06145806412303
+  let precision = 5
+  let sctns = sections(north, east, south, west, precision)
+  check sctns.len == 12
+  check sctns[0] == encode(north, west, precision)
+  check sctns[^1] == encode(south, east, precision)
