7719211895633f39f11b6b2e1a6968e39b17d88a
Jesse Shawl <jesse@jesse.sh>
2025-12-12 13:29:00 -0500

    add server tests (#58)

--- server/src/server.nim
+++ server/src/server.nim
@@ -10,56 +10,41 @@ proc handleSignal() {.noconv.} =
 
 setControlCHook(handleSignal)
 
-proc handleGetYears (req: Request, db: DbConn) {.async.} =
-  let headers = {"Content-type": "application/json; charset=utf-8"}
-  let years = db.findYears()
-  let jsonObj = %* years
-  await req.respond(Http200, jsonObj.pretty(), headers.newHttpHeaders())
-
-proc handleGetMonths (req: Request, db: DbConn, year: string) {.async.} =
-  let headers = {"Content-type": "application/json; charset=utf-8"}
-  let months = db.findMonths(year)
-  let jsonObj = %* months
-  await req.respond(Http200, jsonObj.pretty(), headers.newHttpHeaders())
-
-proc handleGetDays (req: Request, db: DbConn, year: string, month: string) {.async.} =
-  let headers = {"Content-type": "application/json; charset=utf-8"}
-  let days = db.findDays(year, month)
-  let jsonObj = %* days
-  await req.respond(Http200, jsonObj.pretty(), headers.newHttpHeaders())
-
-proc handleGetGeoHashes (req: Request, db: DbConn, north, east, south, west: float, precision: int) {.async.} =
-  let headers = {"Content-type": "application/json; charset=utf-8"}
-  let hashes = db.findGeoHashes(north, east, south, west, precision)
-  let jsonObj = %* hashes
-  await req.respond(Http200, jsonObj.pretty(), headers.newHttpHeaders())
+proc handleRequest*(db: DbConn, path: string, queryParams: Table[string, string]): (HttpCode, JsonNode) =
+  case path
+  of "/api/years":
+    let years = db.findYears()
+    return (Http200, %* years)
+  of "/api/geohashes":
+    let north = parseFloat(queryParams["north"])
+    let east = parseFloat(queryParams["east"])
+    let south = parseFloat(queryParams["south"])
+    let west = parseFloat(queryParams["west"])
+    let precision = parseInt(queryParams["precision"])
+    let hashes = db.findGeoHashes(north, east, south, west, precision)
+    return (Http200, %* hashes)
+  of "/api/months":
+    let months = db.findMonths(queryParams["year"])
+    return (Http200, %* months)
+  of "/api/days":
+    let days = db.findDays(queryParams["year"], queryParams["month"])
+    return (Http200, %* days)
+  of "/api":
+    let events = db.findMultipleEvents(queryParams["from"], queryParams["to"])
+    return (Http200, %* events)
+  else:
+    return (Http404, %* "Not found")
 
 proc main {.async.} =
   var server = newAsyncHttpServer()
   let db = setupDb("db/")
   proc cb(req: Request) {.async.} =
-    if req.url.path == "/api/years":
-      await handleGetYears(req, db)
     var queryParams = initTable[string, string]()
     for key, value in decodeQuery(req.url.query):
       queryParams[key] = value
-    var jsonObj = %* []
-    if req.url.path == "/api/geohashes":
-      let north = parseFloat(queryParams["north"])
-      let east = parseFloat(queryParams["east"])
-      let south = parseFloat(queryParams["south"])
-      let west = parseFloat(queryParams["west"])
-      let precision = parseInt(queryParams["precision"])
-      await handleGetGeoHashes(req, db, north, east, south, west, precision)
-    if req.url.path == "/api/months" and queryParams.hasKey("year"):
-      await handleGetMonths(req, db, queryParams["year"])
-    if req.url.path == "/api/days" and queryParams.hasKey("year") and queryParams.hasKey("month"):
-      await handleGetDays(req, db, queryParams["year"], queryParams["month"])
-    if queryParams.hasKey("from") and queryParams.hasKey("to"):
-      let events = db.findMultipleEvents(queryParams["from"], queryParams["to"])
-      jsonObj = %* events
+    let (code, body) = handleRequest(db, req.url.path, queryParams)
     let headers = {"Content-type": "application/json; charset=utf-8"}
-    await req.respond(Http200, jsonObj.pretty(), headers.newHttpHeaders())
+    await req.respond(code, body.pretty(), headers.newHttpHeaders())
 
   server.listen(Port(8080)) # or Port(8080) to hardcode the standard HTTP port.
   let port = server.getPort
@@ -73,4 +58,5 @@ proc main {.async.} =
       await sleepAsync(500)
   db.closeConnection()
 
-waitFor main()
+when isMainModule:
+  waitFor main()
---
+++ server/tests/test_server.nim
@@ -0,0 +1,53 @@
+import unittest
+import ../src/server
+import ../src/database
+import std / [asynchttpserver, tables, os, times, tempfiles]
+import json
+
+suite "server":
+    setup:
+        let tmpDir = createTempDir("tmp", "", "db")
+        let db = setupDb(tmpDir & "/")
+        db.insert(Event(
+            created_at: parse("2000-01-01T00:00:00-05:00", "yyyy-MM-dd'T'HH:mm:sszzz", utc()),
+            lat: 1.23456,
+            lon: 4.56789
+        ))
+    teardown:
+        removeDir(tmpDir)
+        db.closeConnection()
+    test "/api/years":
+        let (code, body) = handleRequest(db, "/api/years", initTable[string, string]())
+        check code == Http200
+        let data = %*[{"year": 2000, "count": 1}]
+        check body == data
+    test "/api/geohashes":
+        let (code, body) = handleRequest(db, "/api/geohashes", {
+          "north": "3.0",
+          "south": "1.0",
+          "east": "5.0",
+          "west": "4.0",
+          "precision": "5"
+        }.toTable)
+        check code == Http200
+        let data = %*["s05pp"]
+        check body == data
+    test "/api/months":
+        let (code, body) = handleRequest(db, "/api/months", {"year": "2000"}.toTable)
+        check code == Http200
+        let data = %*[{"month": "2000-01", "count": 1}]
+        check body == data
+    test "/api/days":
+        let (code, body) = handleRequest(db, "/api/days", {"year": "2000", "month": "01"}.toTable)
+        check code == Http200
+        let data = %*[{"day": "2000-01-01", "count": 1}]
+        check body == data
+    test "/api":
+        let (code, body) = handleRequest(db, "/api", {"from": "2000-01-01", "to": "2000-01-02"}.toTable)
+        check code == Http200
+        let data = %*[{
+           "created_at": "2000-01-01T05:00:00+00:00",
+           "lat": 1.23456,
+           "lon": 4.56789
+        }]
+        check body == data
